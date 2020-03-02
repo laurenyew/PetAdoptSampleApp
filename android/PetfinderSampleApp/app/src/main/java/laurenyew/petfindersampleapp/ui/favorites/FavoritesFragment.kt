df8e4b82ef6f2efc3548d23d45066daf08282c58
Lauren Yew <laurenyew@gmail.com>
2020-03-01 20:07:11 -0600

    Clean up gradle dependencies

--- android/PetfinderSampleApp/app/src/main/java/laurenyew/petfindersampleapp/ui/favorites/FavoritesFragment.kt
+++ android/PetfinderSampleApp/app/src/main/java/laurenyew/petfindersampleapp/ui/favorites/FavoritesFragment.kt
@@ -1,4 +1,4 @@
-package laurenyew.petfindersampleapp.ui.notifications
+package laurenyew.petfindersampleapp.ui.favorites
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import laurenyew.petfindersampleapp.R
 
-class NotificationsFragment : Fragment() {
+class FavoritesFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private lateinit var favoritesViewModel: FavoritesViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(this, Observer {
+        favoritesViewModel =
+            ViewModelProviders.of(this).get(FavoritesViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_favorites, container, false)
+        val textView: TextView = root.findViewById(R.id.text_favorites)
+        favoritesViewModel.text.observe(this, Observer {
             textView.text = it
         })
         return root
--- android/PetfinderSampleApp/app/src/main/java/laurenyew/petfindersampleapp/ui/favorites/FavoritesViewModel.kt
+++ android/PetfinderSampleApp/app/src/main/java/laurenyew/petfindersampleapp/ui/favorites/FavoritesViewModel.kt
@@ -1,13 +1,13 @@
-package laurenyew.petfindersampleapp.ui.dashboard
+package laurenyew.petfindersampleapp.ui.favorites
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class FavoritesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is Favorites Fragment"
     }
     val text: LiveData<String> = _text
 }
