f3db751bb913b646a1a209b38aa33cf1ba07df59
Lauren Yew <laurenyew@gmail.com>
2021-05-23 13:14:45 -0500

    Fix search view model

--- android/PetfinderSampleApp/app/src/main/java/laurenyew/petfindersampleapp/ui/PetFinderMainScreen.kt
+++ android/PetfinderSampleApp/app/src/main/java/laurenyew/petfindersampleapp/ui/PetFinderMainScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltNavGraphViewModel
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -96,7 +97,7 @@ fun PetFinderMainContent(
             HomeScreen()
         }
         composable(DrawerScreens.Search.route) {
-            val searchViewModel: PetSearchViewModel = hiltNavGraphViewModel()
+            val searchViewModel = hiltViewModel<PetSearchViewModel>()
             PetSearchScreen(
                 viewModel = searchViewModel
             )
