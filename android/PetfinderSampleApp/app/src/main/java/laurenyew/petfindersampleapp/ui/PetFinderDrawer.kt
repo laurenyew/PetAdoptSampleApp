f7488734b728d84031726f1fd328592a156389e8
Lauren Yew <laurenyew@gmail.com>
2021-05-23 13:18:23 -0500

    Fix up drawer layout

--- android/PetfinderSampleApp/app/src/main/java/laurenyew/petfindersampleapp/ui/PetFinderDrawer.kt
+++ android/PetfinderSampleApp/app/src/main/java/laurenyew/petfindersampleapp/ui/PetFinderDrawer.kt
@@ -3,14 +3,13 @@ package laurenyew.petfindersampleapp.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import laurenyew.petfindersampleapp.R
@@ -36,22 +35,26 @@ fun PetFinderDrawer(
     Column(
         modifier
             .fillMaxSize()
-            .padding(start = 24.dp, top = 48.dp)
+            .padding(top = 48.dp)
     ) {
         Image(
             painter = painterResource(R.drawable.ic_logo),
             contentDescription = "App icon",
+            contentScale = ContentScale.Fit,
             modifier = Modifier.fillMaxWidth()
         )
+        Spacer(modifier = Modifier.height(8.dp))
         Divider(color = dividerColor)
         screens.forEach { screen ->
             Spacer(Modifier.height(24.dp))
             Text(
                 text = screen.route,
                 style = MaterialTheme.typography.h4,
-                modifier = Modifier.clickable {
-                    onScreenSelected(screen.route)
-                }
+                modifier = Modifier
+                    .padding(start = 24.dp)
+                    .clickable {
+                        onScreenSelected(screen.route)
+                    }
             )
         }
     }
